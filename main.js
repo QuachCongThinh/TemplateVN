0bebd7c47add3fa0f0f9177bd2c4983197707a7e
QuachCongThinh <qcthinh3105@gmail.com>
2023-10-30 09:25:30 +0700

    Update

--- main.js
+++ main.js
@@ -2,14 +2,15 @@ let openMenu = document.querySelector('.navDiv');
 let menu = document.querySelector('#header .nav .navbar .navXmark');
 let closeMenu = document.querySelector('#header .nav .navbar .navXmark label');
 
+//Handle Open Menu button
 openMenu.onclick = function(){
     menu.style.width = '100%';
 }
-
+//Handle Close Menu button
 closeMenu.onclick = function(){
     menu.style.width = '0';
 }
-
+//Hide menu on click
 let menuItems = document.querySelectorAll('#header .nav .navbar ul li a');
 let body = document.querySelector('body');
 
@@ -27,15 +28,15 @@ for (let i = 0; i < menuItems.length; i++) {
     }   
 }
 
+//Handle Header Events
 let headerScroll = document.querySelector('#header .menuscroll')
 
-const scroll = () => {
-    console.log(window.scrollY);
+const headerFixed = () => {
     if(window.scrollY > 100){
-        headerScroll.style.display = 'block'
+        headerScroll.style.transform = 'translateY(0%)';
+        headerScroll.style.transition = 'all 0.2s';
     }else{
-        headerScroll.style.display = 'none'
+        headerScroll.style.transform = 'translateY(-100%)';
     }
 }
-window.addEventListener('scroll', scroll)
-
+window.addEventListener('scroll', headerFixed)
