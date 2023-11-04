bab27f6806775eec2e3a17b3d8099a3cc54207f5
QuachCongThinh <qcthinh3105@gmail.com>
2023-11-04 22:21:51 +0700

    Update

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-let openMenu = document.querySelector('.navDiv'); 
+let openMenu = document.querySelector('.navDiv');
 let menu = document.querySelector('#header .nav .navbar .navXmark');
 let closeMenu = document.querySelector('#header .nav .navbar .navXmark label');
 
 //Handle Open Menu button
-openMenu.onclick = function(){
+openMenu.onclick = function () {
     menu.style.width = '100%';
-}
+};
 //Handle Close Menu button
-closeMenu.onclick = function(){
+closeMenu.onclick = function () {
     menu.style.width = '0';
-}
+};
 //Hide menu on click
 let menuItems = document.querySelectorAll('#header .nav .navbar ul li a');
 let body = document.querySelector('body');
@@ -18,25 +18,24 @@ for (let i = 0; i < menuItems.length; i++) {
     let menuItem = menuItems[i];
     let currentWidth = body.clientWidth;
 
-    menuItem.onclick = function(){
-        if(currentWidth <= 768){
+    menuItem.onclick = function () {
+        if (currentWidth <= 768) {
             menu.style.width = '100%';
-        }
-        else if(currentWidth > 768){
+        } else if (currentWidth > 768) {
             menu.style.width = null;
         }
-    }   
+    };
 }
 
 //Handle Header Events
-let headerScroll = document.querySelector('#header .menuscroll')
+let headerScroll = document.querySelector('#header .menuscroll');
 
-const headerFixed = () => {
+const headerFixed = () => {  
     if(window.scrollY > 100){
+        headerScroll.style.transition = 'all 0.3s';
         headerScroll.style.transform = 'translateY(0%)';
-        headerScroll.style.transition = 'all 0.2s';
     }else{
         headerScroll.style.transform = 'translateY(-100%)';
     }
-}
-window.addEventListener('scroll', headerFixed)
+};
+window.addEventListener('scroll', headerFixed);
